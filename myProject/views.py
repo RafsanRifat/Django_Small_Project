3b4eb7e1c8fb986e688afa22896ca92d2601067a
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-20 17:28:48 +0600

    views file added

---
+++ myProject/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return HttpResponse('This is a home page')
+
+def about(request):
+    return HttpResponse('This is a home page')
+
+def contact(request):
+    return HttpResponse('This is a home page')
