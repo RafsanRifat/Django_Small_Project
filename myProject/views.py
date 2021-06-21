6c381d2a44ab2a87f03e8636f545acd6c44d5001
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-21 15:27:15 +0600

    link added

--- myProject/views.py
+++ myProject/views.py
@@ -5,11 +5,3 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('This is a home page')
 
-def about(request):
-    return HttpResponse('This is a about page')
-
-def contact(request):
-    return HttpResponse('This is a contact page')
-
-def rafsan(request):
-    return HttpResponse('This is the inner page')
