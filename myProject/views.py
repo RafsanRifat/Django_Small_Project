c9f1b672f23a641c3b27e8d7164745b98d426445
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-24 18:06:31 +0600

    new url added

--- myProject/views.py
+++ myProject/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 
 def home(request):
-
     return render(request, 'index.html')
+
+# def about(request):
+#     return render(request, 'index.html')
+
+def about(request):
+    return HttpResponseRedirect(request.about)
+
 
 
 
