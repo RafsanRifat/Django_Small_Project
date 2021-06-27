88afe5003f4db098227b3edadb2468d94a8966ed
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-27 16:37:27 +0600

    protfolio app added

--- myProject/urls.py
+++ myProject/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about/', views.about, name= 'about')
+    path('', include('protfolio.urls') ,name='home'),
+    # path('about/', views.about, name= 'about')
 
 ]
