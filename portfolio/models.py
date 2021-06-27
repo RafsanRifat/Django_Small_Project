da061c91a2132d3cf4f5809973e6c6eeb5e1242c
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-27 18:07:28 +0600

    new field added to the  database

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class HeroSection(models.Model):
+    image = models.ImageField(upload_to='hero_image/', default=1)
+    name = models.CharField(max_length=100, default=1)
+    title = models.TextField(max_length=200, default=1)
