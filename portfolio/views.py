8f36c3f77145208f171643ed57029ee8c0454607
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-06-29 13:25:03 +0600

    data object created and passed

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import HeroSection
 
 # Create your views here.
 
 def portfolio(request):
-    return render(request, 'index.html')
+    herodata = HeroSection.objects.all()
+    context  = {
+        'hero':herodata
+    }
+    return render(request, 'index.html',context)
